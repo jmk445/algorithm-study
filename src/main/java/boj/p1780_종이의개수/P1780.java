384a5f1b47377ae2117c761778e22d2fbc5e61c1
jmk445 <jmk445123456@gmail.com>
2025-07-07 16:18:51 +0900

    solved : P1780

--- src/main/java/boj/p1780_종이의개수/P1780.java
+++ src/main/java/boj/p1780_종이의개수/P1780.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
 public class P1780 {
     static int sameCnt = 0;
@@ -14,15 +15,20 @@ public class P1780 {
         int N = Integer.parseInt(br.readLine());
 
         int[][] map = new int[N][N];
-
+        for(int i = 0; i < N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < N; j ++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
         sameCnt = 0;
-        recursion(map, N, 1);
+        recursion(map, N, -1);
         System.out.println(sameCnt);
         sameCnt = 0;
         recursion(map, N, 0);
         System.out.println(sameCnt);
         sameCnt = 0;
-        recursion(map, N, -1);
+        recursion(map, N, 1);
         System.out.println(sameCnt);
 
     }
@@ -34,7 +40,7 @@ public class P1780 {
             }
             return;
         }
-        int splitLength = 3/N;
+        int splitLength = N/3;
         List<int[][]> splitedMaps = new ArrayList<int[][]>();
         for(int i = 0; i < 9;i ++){
             int[][] splited = new int[splitLength][splitLength];
@@ -49,7 +55,7 @@ public class P1780 {
         }
 
         for(int i = 0; i < splitedMaps.size(); i++){
-            recursion(splitedMaps.get(i),);
+            recursion(splitedMaps.get(i),N/3,target);
         }
     }
     static boolean allSame(int[][] map){
