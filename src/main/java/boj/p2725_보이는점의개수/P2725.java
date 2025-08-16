08ec3ded45957eb81cc66fc266ea195c1df3a246
jmk445 <jmk445123456@gmail.com>
2025-08-16 17:26:07 +0900

    solved : p2725

--- src/main/java/boj/p2725_보이는점의개수/P2725.java
+++ src/main/java/boj/p2725_보이는점의개수/P2725.java
@@ -8,9 +8,9 @@ public class P2725 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-        int[] dp = new int[1000];
-        dp[1] = 3;
-        dp[2] = 5;
+        Long[] dp = new Long[1000];
+        dp[1] = 3L;
+        dp[2] = 5L;
         int[] testCases = new int[T];
         int max = 0;
         for(int i = 0; i < T; i++){
@@ -18,14 +18,18 @@ public class P2725 {
             max = Integer.max(max,testCases[i]);
         }
 
-        for(int i = 3; i < max; i++){
+        for(int i = 3; i <= max; i++){
             int serosoCnt =  0;
             for(int j = 0; j <  i; j++){
                 if(gcd(i,j) == true){
                     serosoCnt++;
                 }
             }
-            dp[i] = dp[i-1] + serosoCnt * 2;
+            dp[i] = dp[i-1] + serosoCnt * 2L;
+        }
+
+        for(int i = 0; i < T; i++){
+            System.out.println(dp[testCases[i]]);
         }
     }
 
