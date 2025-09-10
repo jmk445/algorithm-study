b48d87c90b5de89e172c98105efff5e5c1b1fe1a
jmk445 <jmk445123456@gmail.com>
2025-09-10 16:59:08 +0900

    solving : p1270

---
+++ src/main/java/boj/p1270_땅따먹기/P1270.java
@@ -0,0 +1,24 @@
+package boj.p1270_땅따먹기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class P1270 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for(int i = 0; i < N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+        }
+
+
+    }
+}
--- src/main/java/boj/p1448_삼각형만들기/P1448_Ans.java
+++ src/main/java/boj/p1448_삼각형만들기/P1448_Ans.java
@@ -3,40 +3,33 @@ package boj.p1448_삼각형만들기;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
 
 public class P1448_Ans {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         Integer N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
+        Integer[] arr = new Integer[N];
         for(int i = 0; i < N; i ++){
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        Arrays.sort(arr);
+        Arrays.sort(arr,(a,b)->Integer.compare(b,a));
 
-        Queue<Integer> queue = new ArrayDeque<>();
-
-        queue.offer(arr[0]);
-        queue.offer(arr[1]);
-        queue.offer(arr[2]);
+//        System.out.println(Arrays.toString(arr));
 
         for(int i = 0; i < N - 2; i++){
-            int b1 = queue.peek();
-            int b2 = queue.peek();
-            int b3 = queue.peek();
 
-            if(b1 < b2 + b3){
-                System.out.println(b1+b2+b3);
+            if(arr[i] < arr[i+1] + arr[i+2]){
+                System.out.println(arr[i]+arr[i+1]+arr[i+2]);
                 return;
             }
-
-            queue.offer(arr[i]);
-            queue.offer(arr[i+1]);
-            queue.offer(arr[i+2]);
         }
+
+        System.out.println("-1");
 
     }
 }
